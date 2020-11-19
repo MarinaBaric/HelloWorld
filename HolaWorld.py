db446af89b8ace4338eeccaed504d7d1b62c7994
Marina Baric <marina.baric.external@veoneer.com>
2020-11-19 16:23:13 +0100

    Added product and sumsum code

--- HolaWorld.py
+++ HolaWorld.py
@@ -1,3 +1,15 @@
-## Hello World
+import os
+import sys
 
-print("Hola amigos")
+print('Hola World')
+
+first_number = input('Please provide a number: ')
+second_number = input('One more please: ')
+product = int(first_number) * int(second_number)
+sumsum = int(first_number) + int(second_number)
+
+if product < 1000:
+    print(product)
+else: 
+    print(sumsum)
+    print('Do you understand?')
