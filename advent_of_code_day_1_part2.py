1a96f43f18941abb79d4ea0d3d0cee6bcc8e849d
Marina Baric <marina.baric.external@veoneer.com>
2021-01-08 14:01:28 +0100

    finished da part 2

--- advent_of_code_day_1_part2.py
+++ advent_of_code_day_1_part2.py
@@ -1,40 +1,15 @@
 import os
 import sys
+from itertools import combinations 
 
+#NumList = [1721,299,55,77,99,675,1456,366,44,675,44,3333,22,979,55,445]
+#test_list = [1721,299,55,77,99,675,1456,366,44,675,44,3333,22,979,55,445] 
 
-NumList = [1721,299,55,77,99,675,1456,366,44,675,44,3333,22,97966,55,445]
+sum = 2020
 
-#a = input("Enter elements: ").split()
-#for i in range(len(a)):
-#    a[i] = int(a[i])
-#print(a)
-'''
 NumList = open('elements.txt').read().splitlines()
 for i in range(len(NumList)):
     NumList[i] = int(NumList[i])
-#print(NumList)
-'''
-
-
-def control_sum(mylist):
-    x=0
-    y=x+1
-    new_list = [i+mylist[x] for i in mylist]
-    new_new_list = [i+mylist[y] for i in new_list]
-    if 2020 in new_list:
-        print(mylist[x],"+",mylist[new_list.index(2020)],"=",mylist[x]+mylist[new_list.index(2020)])
-        print(mylist[x],"*",mylist[new_list.index(2020)],"=",mylist[x]*mylist[new_list.index(2020)])
-    else:
-        x = x+1
-        print("bajs")
-        control_sum(mylist[x:])
-    return control_sum
-
-control_sum(NumList)
-
-
-
-
 
 '''
 sum of of three entries in list python
@@ -46,4 +21,20 @@ https://www.geeksforgeeks.org/python-three-element-sum-in-list/
 
 
 
+def control_sum(mylist):
+    for i in range(0, len(mylist)-2):
+        for j in range(i+1, len(mylist)-1):
+            for k in range(j+1, len(mylist)):
+                if mylist[i] + mylist[j] + mylist[k] == sum:
+                   mynewlist = []
+                   mynewlist.append(mylist[i])
+                   mynewlist.append(mylist[j])
+                   mynewlist.append(mylist[k])
+                   print("Summation of three elementts is : ", mynewlist)
+                   print("Product of the three elements is : ",(mynewlist[0] * mynewlist[1] * mynewlist[2]))
+    return control_sum
+    
+    
+control_sum(NumList)
 
+ 
